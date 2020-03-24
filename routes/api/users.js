57ac1fa6ca62a5f364b4e65db50a245a3825634d
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-24 11:29:56 -0400

    Created request & Body Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-//@route    GET api/users
-//@desc     Test Route
+//@route    POST api/users
+//@desc     User Registration
 //@access   Public
-router.get('/', (req, res) => res.send('User Route'));
+router.post(
+  '/',
+  [
+    check('name')
+      .not()
+      .isEmpty()
+      .withMessage('Name is required'),
+    check('email')
+      .isEmail()
+      .withMessage('Please enter valid email'),
+    check('password')
+      .isLength({ min: 6 })
+      .withMessage('Please enter a password with 6 or more characters')
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User Route');
+  }
+);
 
 module.exports = router;
