449a42b27520f26d93594d6e35c2aa6db54b6ee6
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-24 17:34:22 -0400

    Added profile experince add

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -157,4 +157,63 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+//@route    PUT api/profile/experience
+//@desc     Add Profile experience
+//@access   Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title')
+        .not()
+        .isEmpty()
+        .withMessage('Title is required'),
+      check('company')
+        .not()
+        .isEmpty()
+        .withMessage('Company is required'),
+      check('from')
+        .not()
+        .isEmpty()
+        .withMessage('From date is required')
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
