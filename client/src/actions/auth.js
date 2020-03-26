24824a8d3c1da5602f05dac2e1d903fd4b000ad4
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-26 14:48:55 -0400

    added for Dashboard for created profile and get Profile

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,7 +8,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from './types';
 
 //Load User
@@ -82,5 +83,6 @@ export const login = (email, password) => async dispatch => {
 
 //LOGOUT and clear Profile
 export const logout = () => dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
